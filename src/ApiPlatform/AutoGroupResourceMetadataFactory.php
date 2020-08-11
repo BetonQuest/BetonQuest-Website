730f9b26cea1079832745aa6ad7f59a6f937f16a
Niklas Seyfarth <niklas@seyfarth.de>
2020-08-11 11:14:26 +0200

    Refactor AutoGroupResourceMetadataFactory to not use magic values.

--- src/ApiPlatform/AutoGroupResourceMetadataFactory.php
+++ src/ApiPlatform/AutoGroupResourceMetadataFactory.php
@@ -4,11 +4,17 @@
 namespace App\ApiPlatform;
 
 
+use ApiPlatform\Core\Exception\ResourceClassNotFoundException;
 use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
 use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
 
 class AutoGroupResourceMetadataFactory implements ResourceMetadataFactoryInterface
 {
+    private const ACTION_READ = 'read';
+    private const ACTION_WRITE = 'write';
+    private const SCOPE_ITEM = 'item';
+    private const SCOPE_COLLECTION = 'collection';
+
     /**
      * @var ResourceMetadataFactoryInterface
      */
@@ -23,57 +29,99 @@ class AutoGroupResourceMetadataFactory implements ResourceMetadataFactoryInterfa
         $this->decorated = $decorated;
     }
 
+    /**
+     * @param string $resourceClass
+     * @return ResourceMetadata
+     * @throws ResourceClassNotFoundException
+     */
     public function create(string $resourceClass): ResourceMetadata
     {
         $resourceMetadata = $this->decorated->create($resourceClass);
 
         $itemOperations = $resourceMetadata->getItemOperations();
         $resourceMetadata = $resourceMetadata->withItemOperations(
-            $this->updateContextOnOperations($itemOperations, $resourceMetadata->getShortName(), true)
+            $this->updateContextOnOperations($itemOperations, $resourceMetadata->getShortName(), self::SCOPE_ITEM)
         );
         $collectionOperations = $resourceMetadata->getCollectionOperations();
         $resourceMetadata = $resourceMetadata->withCollectionOperations(
-            $this->updateContextOnOperations($collectionOperations, $resourceMetadata->getShortName(), false)
+            $this->updateContextOnOperations($collectionOperations, $resourceMetadata->getShortName(), self::SCOPE_COLLECTION)
         );
 
         return $resourceMetadata;
     }
 
-    private function updateContextOnOperations(array $operations, string $shortName, bool $isItem)
+    private function updateContextOnOperations(array $operations, string $element, string $scope)
     {
+        $element = strtolower($element);
         foreach ($operations as $operationName => $operationOptions) {
             $operationOptions['normalization_context'] = $operationOptions['normalization_context'] ?? [];
             $operationOptions['normalization_context']['groups'] = $operationOptions['normalization_context']['groups'] ?? [];
             $operationOptions['normalization_context']['groups'] = array_unique(array_merge(
                 $operationOptions['normalization_context']['groups'],
-                $this->getDefaultGroups($shortName, true, $isItem, $operationName)
+                $this->getDefaultGroups($element, $scope, self::ACTION_READ, $operationName)
             ));
             $operationOptions['denormalization_context'] = $operationOptions['denormalization_context'] ?? [];
             $operationOptions['denormalization_context']['groups'] = $operationOptions['denormalization_context']['groups'] ?? [];
             $operationOptions['denormalization_context']['groups'] = array_unique(array_merge(
                 $operationOptions['denormalization_context']['groups'],
-                $this->getDefaultGroups($shortName, false, $isItem, $operationName)
+                $this->getDefaultGroups($element, $scope, self::ACTION_WRITE, $operationName)
             ));
             $operations[$operationName] = $operationOptions;
         }
         return $operations;
     }
 
-    private function getDefaultGroups(string $shortName, bool $normalization, bool $isItem, string $operationName)
+    /**
+     * @param string $element the elements (short) name
+     * @param string $scope item or collection, but could be any other scope
+     * @param string $action read or write, but could be any abstract action
+     * @param string $operation HTTP operation like get, push or delete
+     * @return string[] array with all relevant groups for given arguments
+     */
+    private function getDefaultGroups(string $element, string $scope, string $action, string $operation): array
     {
-        $shortName = strtolower($shortName);
-        $readOrWrite = $normalization ? 'read' : 'write';
-        $itemOrCollection = $isItem ? 'item' : 'collection';
         return [
-            // {shortName}:{read/write}
-            // e.g. user:read
-            sprintf('%s:%s', $shortName, $readOrWrite),
-            // {shortName}:{item/collection}:{read/write}
-            // e.g. user:collection:read
-            sprintf('%s:%s:%s', $shortName, $itemOrCollection, $readOrWrite),
-            // {shortName}:{item/collection}:{operationName}
-            // e.g. user:collection:get
-            sprintf('%s:%s:%s', $shortName, $itemOrCollection, $operationName),
+            $this->getGlobalActionGroup($element, $action),
+            $this->getScopedActionGroup($element, $scope, $action),
+            $this->getOperationGroup($element, $scope, $operation),
         ];
+    }
+
+    /**
+     * Generates a global (unscoped) action group. The group follows the convention {element}:{action}, e.g. user:read.
+     *
+     * @param string $element the elements (short) name
+     * @param string $action read or write, but could be any abstract action
+     * @return string the generated global action group
+     */
+    private function getGlobalActionGroup(string $element, string $action): string
+    {
+        return sprintf('%s:%s', $element, $action);
+    }
+
+    /**
+     * Generates a scoped action group. The group follows the convention {element}:{scope}:{action}, e.g. user:collection:read.
+     *
+     * @param string $element the elements (short) name
+     * @param string $scope item or collection, but could be any other scope
+     * @param string $action read or write, but could be any abstract action
+     * @return string the generated scoped action group
+     */
+    private function getScopedActionGroup(string $element, string $scope, string $action): string
+    {
+        return sprintf('%s:%s:%s', $element, $scope, $action);
+    }
+
+    /**
+     * Generates an operation group. The group follows the convention {element}:{scope}:{operation}, e.g. user:collection:get.
+     *
+     * @param string $element the elements (short) name
+     * @param string $scope item or collection, but could be any other scope
+     * @param string $operation HTTP operation like get, push or delete
+     * @return string the generated operation group
+     */
+    private function getOperationGroup(string $element, string $scope, string $operation): string
+    {
+        return sprintf('%s:%s:%s', $element, $scope, $operation);
     }
 }
